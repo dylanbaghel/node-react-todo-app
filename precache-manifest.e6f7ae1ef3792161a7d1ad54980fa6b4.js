7f2bb25a2e643b390d436114e01397e44f33ddbe
dylanbaghel <baghelabhi39@gmail.com>
2018-10-09 23:22:36 +0530

    Updates

---
+++ precache-manifest.e6f7ae1ef3792161a7d1ad54980fa6b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e2490c67de3a201e23484298593bc61",
+    "url": "/node-react-todo-app/static/media/preloader.7e2490c6.gif"
+  },
+  {
+    "revision": "19f95a80fba8f4bfc343",
+    "url": "/node-react-todo-app/static/js/runtime~main.19f95a80.js"
+  },
+  {
+    "revision": "e56b3fc755641fcb1c16",
+    "url": "/node-react-todo-app/static/js/main.e56b3fc7.chunk.js"
+  },
+  {
+    "revision": "a532b0ea478a51671587",
+    "url": "/node-react-todo-app/static/js/1.a532b0ea.chunk.js"
+  },
+  {
+    "revision": "e56b3fc755641fcb1c16",
+    "url": "/node-react-todo-app/static/css/main.bcc1ad97.chunk.css"
+  },
+  {
+    "revision": "a532b0ea478a51671587",
+    "url": "/node-react-todo-app/static/css/1.807010f4.chunk.css"
+  },
+  {
+    "revision": "eb785cfe7ac111ed6f3beb6b455253e4",
+    "url": "/node-react-todo-app/index.html"
+  }
+];
